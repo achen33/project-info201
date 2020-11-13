6b812a769fe69af57880218ae0581f9a1e4ac91a
Hadar Dolev <hadardolev@Hadars-MacBook-Pro.local>
2020-11-12 18:50:02 -0800

    table with attempt at group by

--- scripts:/summaryInformationTable.r
+++ scripts:/summaryInformationTable.r
@@ -1,13 +1,39 @@
 library(readxl)
 library(tidyr)
+library(dplyr)
 install.packages("installr")
 
+#state table 
+total_in_state <- table04b %>%
+  select(X1, X2, X3, X5, X10) %>%
+  rename(
+    state = X1,
+    sex_race = X2,
+    total_population = X3,
+    total_registered = X5,
+    total_voted = X10
+  ) %>%
+  slice(17: 577) 
 
-total_pop <- voting_table %>%
-  select(X2, X3, X5, X10) %>%
-  slice(5:11) 
-  names(total_pop)[1] <- "sex_race"
-  names(total_pop)[2] <- "total_population"
-  names(total_pop)[3] <- "total_registered"
-  names(total_pop)[4] <- "total_voted"
+#US total table   
+us_table <- table04b %>%
+  select(X1, X2, X3, X5, X10) %>%
+  rename(
+    state = X1,
+    sex_race = X2,
+    total_population = X3,
+    total_registered = X5,
+    total_voted = X10
+  ) %>%
+filter(state == "US")
+
+  
+#attempt at US group by   
+grouped_table <- us_table %>%
+  group_by(state) %>%
+  summarise(
+    mean_voted = mean(total_voted)
+  )
+  
+  
   
