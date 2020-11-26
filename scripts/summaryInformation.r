5ac57f5d7cb6c89a9adadd152cd5e4c114bb23d9
Hadar Dolev <hadardolev@hadars-mbp.lan>
2020-11-26 00:52:00 -0800

    new working values

--- scripts/summaryInformation.r
+++ scripts/summaryInformation.r
@@ -15,44 +15,67 @@ library(readr)
 county_statistics_1_ <- read_csv("data/county_statistics (1).csv")
 View(county_statistics_1_)
 
-state_votes <- county_statistics_1_ %>%
-  select (state, percentage20_Joe_Biden, percentage20_Donald_Trump) %>%
+#State with the most  % votes for Biden in the 2020 election 
+summary_info$max_percent_joe <- county_statistics_1_ %>%
+  select (state, percentage20_Joe_Biden) %>%
   group_by(state)%>%
-  summarise(percentage20_Joe_Biden == mean(percentage20_Joe_Biden, na.rm=T)) %>%
-  filter(percentage20_Joe_Biden == min(percentage20_Joe_Biden, na.rm=T)) %>%
-  View(state_votes)
-
-#State with the most votes for Biden in the 2020 election 
-state_max_voters_biden20 <- county_statistics_1_ %>%
-  select(state, percentage20_Joe_Biden) %>%
-  group_by(state) %>%
-  summarise(percentage20_Joe_Biden == max(percentage20_Joe_Biden, na.rm=T)) %>%
+  summarise(max_percent = mean(percentage20_Joe_Biden, na.rm=T)) %>%
+  filter(max_percent == max(max_percent, na.rm=T)) %>%
   pull(state)
+  View(max_percent_joe)
 
-#State with the most votes for Trump in the 2020 election 
-#-------------------FIX? -----------------------
-state_max_voters_trump20 <- county_statistics_1_ %>%
-  select(state, percentage20_Donald_Trump) %>%
-  filter(percentage20_Donald_Trump == max(percentage20_Donald_Trump, na.rm=T)) %>%
-  pull(state)
+
+#State with the most % votes for Trump in the 2020 election 
+summary_info$max_percent_trump <- county_statistics_1_ %>%
+    select (state, percentage20_Donald_Trump) %>%
+    group_by(state)%>%
+    summarise(max_percent = mean(percentage20_Donald_Trump, na.rm=T)) %>%
+    filter(max_percent == max(max_percent, na.rm=T)) %>%
+    pull(state)
+    View(max_percent_trump)
 
 #state with the most black population 
-#State with the most black population 
-race_pop_black <- county_statistics_1_ %>%
-  Select(state, county, TotalPop, Men, Women, Hispanic, White, Black, Native, Asian, Pacific) %>%
-  mutate(black_percent = Black / TotalPop *100)
+summary_info$max_race_pop_black <- county_statistics_1_ %>%
+  mutate(black_percent = Black / TotalPop *100) %>%
+  select(state, county, TotalPop, Men, Women, Hispanic, White, Black, Native, Asian, Pacific, black_percent) %>%
   group_by(state) %>%
-  summarise(max_black_pop = max(black_percent, na.rm = T))
-  summarise(min_black_pop = min(black_precent, na.rm=T))
-  
-#state with the most Hispanic population 
-race_pop_hispanic <- county_statistics_1_ %>%
-  select(state, county, TotalPop, Men, Women, Hispanic, White, Black, Native, Asian, Pacific) %>%
-  mutate(hispanic_percent = Hispanic / TotalPop *100)
-  group_by(state) %>%
-  summarise(max_hispanic_pop = max(hispanic_percent, na.rm = T))
-  
+  summarise(max_pop = max(black_percent, na.rm = T))%>%
+  filter(max_pop == max(max_pop, na.rm=T)) %>%
+  pull(state)
 
+  
+#Percent voter turnout for MS 
+summary_info$voter_turnout_ms <- county_statistics_1_ %>%
+  select(state, county, TotalPop, VotingAgeCitizen) %>%
+  mutate(voter_percent = VotingAgeCitizen / TotalPop *100) %>%
+  group_by(state)%>%
+  summarise(avg_voters = mean(voter_percent, na.rm = T)) %>%
+  filter(state == "MS") %>%
+  pull(round(avg_voters))
+  
+#state with the most white population 
+summary_info$max_race_pop_white <- county_statistics_1_ %>%
+  mutate(white_percent = White / TotalPop *100) %>%
+  select(state, county, TotalPop, white_percent) %>%
+  group_by(state) %>%
+  summarise(max_pop = max(white_percent, na.rm = T))%>%
+  filter(max_pop == max(max_pop, na.rm=T)) %>%
+  pull(state)
+  
+  
+summary_info <- list()
+summary_info$max_percent_joe <- county_statistics_1_ %>%
+summary_info$max_percent_trump <- county_statistics_1_ %>%
+summary_info$max_race_pop_black <- county_statistics_1_ %>%
+summary_info$voter_turnout_ms<- county_statistics_1_ %>%
+summary_info$max_race_pop_white<- county_statistics_1_ %>%
+
+  
+  
+  
+  
+  
+  
   
 
 # Selina's work (Ratio: (1) Female Voted to Total Pop Voted; 
