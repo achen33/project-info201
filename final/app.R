c986528eb741552914d2294a31b2233bb316e103
Audrey Chen <Audrey@Audreys-MacBook-Air.local>
2020-12-06 23:36:35 -0800

    app

---
+++ final/app.R
@@ -0,0 +1,14 @@
+library("shiny")
+library("plotly")
+library("tidyverse")
+library("maps")
+library("mapproj")
+library("RColorBrewer")
+library("leaflet")
+
+
+source("app_ui.R")
+source("app_server.R")
+
+
+shinyApp(ui = ui, server = server)
